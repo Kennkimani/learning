6cbad430430350679bbdcfa9400026828b23ec15
Kennedy Kimani <kenkimani755@gmail.com>
2023-11-24 14:02:22 +0300

    data_transformed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,14 @@ import pandas  as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 import os
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+
+
+
 @dataclass###decorator
 class Dataingestionconfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -43,6 +51,9 @@ class Dataingestion:
         
 if __name__=="__main__":
     obj=Dataingestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
        
